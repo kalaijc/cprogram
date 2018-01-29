191413b22b5c7c85326238afaaae73581c18c150
kalaijc <35915005+kalaijc@users.noreply.github.com>
2018-01-29 13:50:36 +0530

    Create .c

---
+++ .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter the number:");
+scanf("%d", &number);
+
+    // True if the number is perfectly divisible by 2
+    if(number % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    return 0;
+}
